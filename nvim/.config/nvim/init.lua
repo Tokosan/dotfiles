6ff114c31fb85ef4b4628e25b9bd926d191cb8cd
Tokosan <cteiba.al@gmail.com>
2025-11-27 00:30:08 -0300

    nvim: config tabsize

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -62,6 +62,12 @@ vim.o.timeoutlen = 300
 vim.o.splitright = true
 vim.o.splitbelow = true
 
+-- Configure tab and indentation behavior
+--  See `:help 'tabstop'`
+vim.o.tabstop = 2 -- Number of spaces that a <Tab> in the file counts for
+vim.o.shiftwidth = 2 -- Number of spaces to use for each step of (auto)indent
+vim.o.expandtab = true -- Use spaces instead of tabs
+
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
