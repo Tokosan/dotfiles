06a0dc5c2eb6bfb164e3f6594d61834fed70d847
Tokosan <cteiba.al@gmail.com>
2025-11-26 23:52:09 -0300

    nvim: remove lua comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,3 @@
---[[
-    If you don't know anything about Lua, I recommend taking some time to read through
-    a guide. One possible example which will only take 10-15 minutes:
-      - https://learnxinyminutes.com/docs/lua/
-
-    After understanding a bit more about Lua, you can use `:help lua-guide` as a
-    reference for how Neovim integrates Lua.
-    - :help lua-guide
-    - (or HTML version): https://neovim.io/doc/user/lua-guide.html
---]]
 vim.g.lazyvim_check_order = false
 
 -- transparent background
