5434358c43b6acd31a5a9d2e6ddf394302978fbe
Tokosan <cteiba.al@gmail.com>
2025-12-13 09:19:16 -0300

    system: add install.sh with packages

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is a script intended to automatically install all my packages and stuff
+
+# 1. Install necesary packages
+pacman -Syu --no-confirm vi vim nvim zsh ghostty rofi wayland waybar hyprland unzip tree git 
