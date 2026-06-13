9a2dc512785d05f02c9d3067c6bdddd21dfa9b8e
Tokosan <cteiba.al@gmail.com>
2026-06-12 21:53:45 -0400

    zsh cd on yazi exit

--- dot_config/zsh/functions.zsh
+++ dot_config/zsh/functions.zsh
@@ -21,4 +21,13 @@ function cdir {
   else
     mkdir -p "$1" && cd "$1"
   fi
+} 
+
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  command rm -f -- "$tmp"
 }
+
